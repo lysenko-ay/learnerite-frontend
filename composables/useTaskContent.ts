d7d9cd5994648c3ff11583b2d9be6537e7b5b465
AlexTheRegent <alextheregent@gmail.com>
2022-11-07 00:39:55 +0300

    add tasks content display

--- composables/useTaskContent.ts
+++ composables/useTaskContent.ts
@@ -1,8 +1,9 @@
-export const useTaskContent = async () => {
-  const { data } = await useAsyncData('octave-taskbook', async () => {
+export const useTaskContent = async (course: string, type: string, chapterId: string, taskId: string) => {
+  const { data } = await useAsyncData('octave-taskbook-' + chapterId + '-' + taskId, async () => {
     const config = useRuntimeConfig();
-    const data = await $fetch(config.API_BASE_URL + '/content');
-
-    return {};
+    const data = await $fetch(config.API_BASE_URL + '/content/' + course + '/' + type + '/' + chapterId + '/' + taskId);
+    return data;
   });
+
+  return data.value;
 };
--- pages/[course]/[type]/[chapterId]/[taskId]/index.vue
+++ pages/[course]/[type]/[chapterId]/[taskId]/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="taskbook-task">
     <div>{{ index[chapterId + '/' + taskId].title }}</div>
+    <div>{{ task.description }}</div>
     <textarea class="w-full border-2 focus:outline-none font-mono" rows="10" v-model="code"></textarea>
     <button class="border rounded-xl px-4 py-2 shadow hover:bg-gray-50" @click="onSendPressed">
       Проверить решение
@@ -14,9 +15,12 @@
 import { io } from "socket.io-client"
 
 const route = useRoute()
+const course = route.params.course as string
+const type = route.params.type as string
 const chapterId = route.params.chapterId as string
 const taskId = route.params.taskId as string
-const { index } = await useCourseContent('octave', 'taskbook');
+const { index } = await useCourseContent(course, type);
+const task = await useTaskContent(course, type, chapterId, taskId) as any;
 
 const code = ref('')
 
@@ -27,15 +31,21 @@ const onSendPressed = () => {
   }
 
   socket = io('ws://localhost:3030/')
-  socket.emit('register', (data) => {
-    console.log(data);
+  socket.emit('register', { course, type, chapterId, taskId }, (data) => {
+    console.log('register', data);
   })
   socket.on('request-code', (callback) => {
-    console.log('code requested', callback);
-    callback('disp(1)')
+    console.log('code requested');
+    callback(code.value)
   })
   socket.on('last-job-id', (lastJobId) => {
-    console.log('last job it: ', lastJobId);
+    console.log('last job it:', lastJobId);
+  })
+  socket.on('result', (stdout) => {
+    console.log('result:', stdout);
+  })
+  socket.on('error', (error) => {
+    console.log('error:', error);
   })
 
 }
